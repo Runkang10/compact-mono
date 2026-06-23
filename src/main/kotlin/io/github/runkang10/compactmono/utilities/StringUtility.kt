eeb1a85025fa1d82957a868dbbaba34ed34ce3c1
Runkang10 <chen.runkang10@gmail.com>
2026-06-23 10:40:26 +0200

    feat(utility): Add utilities for String

--- src/main/kotlin/io/github/runkang10/compactmono/utilities/StringUtility.kt
+++ src/main/kotlin/io/github/runkang10/compactmono/utilities/StringUtility.kt
@@ -1,2 +1,13 @@
 package io.github.runkang10.compactmono.utilities
 
+fun strings(
+    separator: CharSequence = "\n",
+    vararg s: String,
+) = s.joinToString(separator)
+
+fun strings(
+    separator: CharSequence = "\n",
+    s: Collection<String>
+) = strings(separator, *s.toTypedArray())
+
+fun Iterable<String>.strings(separator: CharSequence = "\n") = joinToString(separator)
