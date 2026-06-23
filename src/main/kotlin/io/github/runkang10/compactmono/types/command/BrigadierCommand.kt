e3571af8104c92211ad15c2a34d89f933b9ad8f3
Runkang10 <chen.runkang10@gmail.com>
2026-06-23 10:39:50 +0200

    feat(types): Add command interface

--- src/main/kotlin/io/github/runkang10/compactmono/types/command/BrigadierCommand.kt
+++ src/main/kotlin/io/github/runkang10/compactmono/types/command/BrigadierCommand.kt
@@ -1,9 +1,8 @@
 package io.github.runkang10.compactmono.types.command
 
-import com.mojang.brigadier.tree.LiteralCommandNode
-import io.papermc.paper.command.brigadier.CommandSourceStack
+import io.github.runkang10.compactmono.commands.Literal
 
 interface BrigadierCommand {
     fun meta(): BrigadierCommandMeta
-    fun execute(): LiteralCommandNode<CommandSourceStack>
+    fun execute(): Literal
 }
