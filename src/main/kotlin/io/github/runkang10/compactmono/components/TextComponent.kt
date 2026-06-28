9b13a8d602aa478d44a4633ea2f44d8398d0b63a
Runkang10 <chen.runkang10@gmail.com>
2026-06-28 11:04:35 +0200

    fix: Builder not found at runtime

--- src/main/kotlin/io/github/runkang10/compactmono/components/TextComponent.kt
+++ src/main/kotlin/io/github/runkang10/compactmono/components/TextComponent.kt
@@ -3,45 +3,18 @@ package io.github.runkang10.compactmono.components
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.format.TextDecoration
 
 fun textComponent(
     content: String,
     color: TextColor? = null,
+    decoration: TextDecoration? = null,
     builder: TextComponent.Builder.() -> Unit = {}
-) = Component.text().content(content).color(color).apply(builder).build()
-
-fun textComponent(
-    value: Int,
-    color: TextColor? = null,
-    builder: TextComponent.Builder.() -> Unit = {}
-) = Component.text(value, color).toBuilder().apply(builder).build()
-
-fun textComponent(
-    value: Long,
-    color: TextColor? = null,
-    builder: TextComponent.Builder.() -> Unit = {}
-) = Component.text(value, color).toBuilder().apply(builder).build()
-
-fun textComponent(
-    value: Float,
-    color: TextColor? = null,
-    builder: TextComponent.Builder.() -> Unit = {}
-) = Component.text(value, color).toBuilder().apply(builder).build()
-
-fun textComponent(
-    value: Double,
-    color: TextColor? = null,
-    builder: TextComponent.Builder.() -> Unit = {}
-) = Component.text(value, color).toBuilder().apply(builder).build()
-
-fun textComponent(
-    value: Char,
-    color: TextColor? = null,
-    builder: TextComponent.Builder.() -> Unit = {}
-) = Component.text(value, color).toBuilder().apply(builder).build()
-
-fun textComponent(
-    value: Boolean,
-    color: TextColor? = null,
-    builder: TextComponent.Builder.() -> Unit = {}
-) = Component.text(value, color).toBuilder().apply(builder).build()
+) = Component.text()
+    .content(content)
+    .color(color)
+    .also {
+        if (decoration != null) it.decorate(decoration)
+    }
+    .apply(builder)
+    .build()
