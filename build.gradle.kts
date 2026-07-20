4772a364164fedec53b7b12af7b1f44a43a8acce
Runkang10 <chen.runkang10@gmail.com>
2026-07-20 17:26:08 +0200

    chore: add skip maven publish configuration if release variable is not found

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,42 +29,44 @@ kotlin {
     jvmToolchain(libs.versions.jdk.get().toInt())
 }
 
-mavenPublishing {
-    publishToMavenCentral(true, DeploymentValidation.PUBLISHED)
+if (System.getenv("release")?.toBoolean() == true) {
+    mavenPublishing {
+        publishToMavenCentral(true, DeploymentValidation.PUBLISHED)
 
-    signAllPublications()
-    checksums(Checksum.MD5, Checksum.SHA1, Checksum.SHA256, Checksum.SHA512)
-    excludeSignatureChecksums(false)
+        signAllPublications()
+        checksums(Checksum.MD5, Checksum.SHA1, Checksum.SHA256, Checksum.SHA512)
+        excludeSignatureChecksums(false)
 
-    coordinates(
-        "io.github.runkang10",
-        "compact-mono",
-        System.getenv("version") ?: error("'version' variable is missing!")
-    )
-    pom {
-        name.set(rootProject.name)
-        description.set("A Kotlin library for Paper plugins.")
-        inceptionYear.set("2026")
-        url.set("https://github.com/Runkang10/compact-mono")
-
-        licenses {
-            license {
-                name.set("MIT License")
-                url.set("https://github.com/Runkang10/compact-mono?tab=MIT-1-ov-file")
-                distribution.set("https://github.com/Runkang10/compact-mono?tab=MIT-1-ov-file")
-            }
-        }
-        developers {
-            developer {
-                id.set("runkang10")
-                name.set("Runkang10")
-                url.set("https://github.com/Runkang10")
-            }
-        }
-        scm {
+        coordinates(
+            "io.github.runkang10",
+            "compact-mono",
+            System.getenv("version") ?: error("'version' variable is missing!")
+        )
+        pom {
+            name.set(rootProject.name)
+            description.set("A Kotlin library for Paper plugins.")
+            inceptionYear.set("2026")
             url.set("https://github.com/Runkang10/compact-mono")
-            connection.set("scm:git:git://github.com/Runkang10/compact-mono.git")
-            developerConnection.set("scm:git:ssh://git@github.com/Runkang10/compact-mono.git")
+
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://github.com/Runkang10/compact-mono?tab=MIT-1-ov-file")
+                    distribution.set("https://github.com/Runkang10/compact-mono?tab=MIT-1-ov-file")
+                }
+            }
+            developers {
+                developer {
+                    id.set("runkang10")
+                    name.set("Runkang10")
+                    url.set("https://github.com/Runkang10")
+                }
+            }
+            scm {
+                url.set("https://github.com/Runkang10/compact-mono")
+                connection.set("scm:git:git://github.com/Runkang10/compact-mono.git")
+                developerConnection.set("scm:git:ssh://git@github.com/Runkang10/compact-mono.git")
+            }
         }
     }
 }
